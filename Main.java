81e6592abf67a34a4608936f2b771788286b6865
123445656 <87911383+123445656@users.noreply.github.com>
2022-06-13 01:47:34 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Account ac1 = new Account(2112, "Gilbert","");
+        Account ac2 = new Account(2113, "Richard", "Checking");
+        Account ac3 = new Account(2114, "Jean", "");
+        Account ac4 = new Account(2115, "Peter","");
+        Account ac5 = new Account(2116, "Fabrice","");
+        Account ac6 = new Account(2117, "Ange","");
+        Account ac7 = new Account(2118, "Bella","");
+        Account ac8 = new Account(2119, "Ariella","");
+        Account ac9 = new Account(2120, "Aline","");
+        Account ac10 = new Account(2121, "Alice","");
+        System.out.println("Account 1: "+ac1.acc());
+        System.out.println("Account 2: "+ac2.acc());
+        System.out.println("Account 3: "+ac3.acc());
+        System.out.println("Account 4: "+ac4.acc());
+        System.out.println("Account 5: "+ac5.acc());
+        System.out.println("Account 6: "+ac6.acc());
+        System.out.println("Account 7: "+ac7.acc());
+        System.out.println("Account 8: "+ac8.acc());
+        System.out.println("Account 9: "+ac9.acc());
+        System.out.println("Account 10: "+ac10.acc());
+sn
+
+    }
+}
